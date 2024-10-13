b95fbb3f73f28cedc6dd0bfcc6b70ba47204f2a6
unknown <faizilyasa16@gmail.com>
2024-10-13 13:06:29 +0700

    baruan

--- public/js/script.js
+++ public/js/script.js
@@ -3,3 +3,29 @@ function confirmLogout() {
         document.getElementById("keluar-app").submit();
     }
 }
+
+    // Function to add/remove active border
+    function setActiveLink(element) {
+        // Remove active class from all span elements
+        document.querySelectorAll('.icon-wrapper').forEach(function(iconWrapper) {
+            iconWrapper.classList.remove('active-border');
+        });
+
+        // Add active class to clicked element
+        element.classList.add('active-border');
+    }
+
+    // Set the default active border on the 'Home' icon
+    document.addEventListener('DOMContentLoaded', function() {
+        const defaultIcon = document.querySelector('.nav-item:first-child .icon-wrapper');
+        if (defaultIcon) {
+            defaultIcon.classList.add('active-border');
+        }
+    });
+
+    // Add click event listener to all icon wrappers
+    document.querySelectorAll('.nav-item .icon-wrapper').forEach(function(iconWrapper) {
+        iconWrapper.addEventListener('click', function() {
+            setActiveLink(this);
+        });
+    });
--- resources/views/backend/layout.blade.php
+++ resources/views/backend/layout.blade.php
@@ -13,6 +13,9 @@
         .nav-link {
             transition: 0.3s;
         }
+        .active-border {
+    border: 2px solid white;
+}
 
         .nav-link:hover {
             background-color: #343a40;
@@ -30,7 +33,7 @@
         <ul class="d-flex align-items-center list-unstyled m-0 w-100">
             <li class="me-auto ms-xxl-3 ms-md-0">
                 <div class="position-fixed bg-success" style="width: 5px; height: 50px; top: 25px; left: 10px;"></div>
-                <h2 class="ms-xxl-1 ms-md-3">Hi, {{ Auth::user()->username }}</h2>
+                <h2 class="ms-xxl-2 ms-md-4">Hi, {{ Auth::user()->username }}</h2>
             </li>
             <li class="d-flex ms-auto me-5 position-relative">
                 <a href="#" class="text-white dropdown-toggle d-flex align-items-center" id="dropdownMenuButton" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -47,42 +50,41 @@
         <div class="row">
             <div class="col-auto bg-primary-dark" style="min-height: 89vh; width: 100px; overflow: hidden;">
                 <hr class="text-white">
-                <ul class="nav nav-pills flex-column mb-auto">
-                    <li class="nav-item text-center">
+                <ul class="nav nav-pills flex-column mb-auto ">
+                    <li class="nav-item text-center mt-3">
                         <a href="#" class="text-white active text-center">
-                            <span class="nav-link  icon-wrapper bg-primary-dark-bit border border-white rounded-2 d-flex justify-content-center align-items-center mx-auto" style="width: 40px; height: 40px;">
-                                <i class=" text-white fa-solid bi-house-door-fill"></i>
+                            <span class="nav-link icon-wrapper bg-primary-dark-bit rounded-2 d-flex justify-content-center align-items-center mx-auto" style="width: 50px; height: 50px;">
+                                <i class="fs-4 text-white fa-solid bi-house-door-fill"></i>
                             </span>
                             <span class="fs-6 mt-2 mb-3  d-inline-block link-underline link-underline-opacity-0">Home</span>
                         </a>
                     </li>
-                
-                    <li class="nav-item text-center">
+                    <li class="nav-item text-center mt-3">
                         <a href="#" class="text-white text-center">
-                            <span class="nav-link  icon-wrapper bg-primary-dark-bit  rounded-2 d-flex justify-content-center align-items-center mx-auto" style="width: 40px; height: 40px;">
-                                <i class=" text-white fa-solid fa-info"></i>
+                            <span class="nav-link  icon-wrapper bg-primary-dark-bit  rounded-2 d-flex justify-content-center align-items-center mx-auto" style="width: 50px; height: 50px;">
+                                <i class="fs-4 text-white fa-solid bi-pie-chart-fill"></i>
                             </span>
-                            <span class="fs-6 mt-2 mb-3 d-inline-block link-underline link-underline-opacity-0">About</span>
+                            <span class="fs-6 mt-2 mb-3 d-inline-block link-underline link-underline-opacity-0">Dashboard</span>
+                        </a>
+                    </li>
+                    <li class="nav-item text-center mt-3">
+                        <a href="#" class="text-white text-center">
+                            <span class="nav-link  icon-wrapper bg-primary-dark-bit  rounded-2 d-flex justify-content-center align-items-center mx-auto" style="width: 50px; height: 50px;">
+                                <i class="fs-4 text-white fa-solid bi-people-fill"></i>
+                            </span>
+                            <span class="fs-6 mt-2 mb-3 d-inline-block link-underline link-underline-opacity-0">Recruit</span>
+                        </a>
+                    </li>
+                    <li class="nav-item text-center mt-3">
+                        <a href="#" class="text-white text-center">
+                            <span class="nav-link  icon-wrapper bg-primary-dark-bit  rounded-2 d-flex justify-content-center align-items-center mx-auto" style="width: 50px; height: 50px;">
+                                <i class="fs-4 text-white fa-solid fa-building"></i>
+                            </span>
+                            <span class="fs-6 mt-2 mb-3 d-inline-block link-underline link-underline-opacity-0">Company</span>
                         </a>
                     </li>
                 
-                    <li class="nav-item text-center">
-                        <a href="#" class="text-white text-center">
-                            <span class="nav-link bg-primary-dark-bit  icon-wrapper rounded-2 d-flex justify-content-center align-items-center mx-auto" style="width: 40px; height: 40px;">
-                                <i class=" text-white fa-solid fa-building"></i>
-                            </span>
-                            <span class="mt-2 mb-3 d-inline-block link-underline link-underline-opacity-0" style="font-size: 0.7rem;">Company</span>
-                        </a>
-                    </li>
-                
-                    <li class="nav-item text-center">
-                        <a href="#" class="text-white text-center">
-                            <span class="nav-link bg-primary-dark-bit icon-wrapper rounded-2 d-block d-flex justify-content-center align-items-center mx-auto" style="width: 40px; height: 40px;">
-                                <i class="text-white fa-solid fa-user"></i>
-                            </span>
-                            <span class="mt-2 mb-3 d-inline-block link-underline link-underline-opacity-0" style="font-size: 0.8rem;">Recruit</span>
-                        </a>
-                    </li>
+
                 </ul>
                 
             </div>
